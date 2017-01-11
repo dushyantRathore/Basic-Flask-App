af43e8bd622d11e45bc121451734acd1cd07f4d7
dushyantRathore <dushyant.bgs@gmail.com>
2017-01-11 08:38:15 +0530

    Main Example Code

---
+++ Code_Main.py
@@ -0,0 +1,35 @@
+from flask import Flask,request,render_template
+
+app = Flask(__name__)
+
+# @ denotes a decorator
+@app.route('/')
+def index():
+    return "Hello World";
+
+@app.route('/profile')
+def profile():
+    return "<h1><u>Welcome to Profile Page</u></h1>" \
+           "<br>How are you ?"
+
+# For strings
+@app.route('/profile/<username>')
+def display_name(username):
+    return "<h1>Welcome " + username + "</h1>"
+
+# For integers
+@app.route('/post/<int:id>')
+def display_ID(id):
+    return "ID is : " + str(id)
+
+# HTTP Methods
+@app.route('/http', methods=['GET', 'POST'])
+def http_method():
+    if request.method == 'POST':
+        return "It was a POST Request"
+    else :
+        return "It was a GET Request"
+
+if __name__ == "__main__":
+    app.run()
+
